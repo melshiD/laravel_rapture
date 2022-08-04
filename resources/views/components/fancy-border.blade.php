9f58d726329de046bb897a2abf624c8eb2d516ca
David Melsheimer <dave.melshman@gmail.com>
2022-08-04 17:58:31 -0400

    initial commit

---
+++ resources/views/components/fancy-border.blade.php
@@ -0,0 +1,9 @@
+<div class="w-full h-3 bg-red-600 overflow-hidden border-t-2 border-gray-800">
+    <div class="w-40 h-40 bg-red-800 relative left-0 transform skew-x-[25deg]"></div>
+    <div class="w-40 h-40 bg-red-700 relative left-0 transform skew-x-[25deg]"></div>
+    <div class="w-40 h-40 bg-red-600 relative left-0 transform skew-x-[25deg]"></div>
+    <div class="w-40 h-40 bg-red-500 relative left-0 transform skew-x-[25deg]"></div>
+    <div class="w-40 h-40 bg-red-400 relative left-0 transform skew-x-[25deg]"></div>
+</div>
+<!-- WHEN YOU SIT BACK DOWN, install Tailwindcss as a package and not a CDN, then build some 
+custom animations  -->
