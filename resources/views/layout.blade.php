0dc03f9741750cccb453357ceb7a2b7b25ee3e91
David Melsheimer <dave.melshman@gmail.com>
2022-08-07 21:44:54 -0400

    install tailwind rather than CDN

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <title>Rapture Layout Template</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
